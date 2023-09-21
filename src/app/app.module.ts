ef53acc5145f31b2e259a8196648930c6cf8245e
paulolatta <paulogn.lata@gmail.com>
2023-09-21 17:58:41 -0300

    merge issue_866axdtun add simple gallery page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { CardServicesComponent } from './components/card-services/card-services.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { GalleryModule } from 'ng-gallery';
 import { HomeComponent } from './pages/home/home.component';
+import { IconComponent } from './components/icon/icon.component';
+import { LocationComponent } from './pages/location/location.component';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -14,12 +20,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { NgModule } from '@angular/core';
 import { ServicesComponent } from './pages/services/services.component';
-import { GalleryComponent } from './pages/gallery/gallery.component';
-import { LocationComponent } from './pages/location/location.component';
-import { IconComponent } from './components/icon/icon.component';
-import { CardServicesComponent } from './components/card-services/card-services.component';
 
 @NgModule({
   declarations: [
@@ -27,11 +28,11 @@ import { CardServicesComponent } from './components/card-services/card-services.
     HomeComponent,
     ServicesComponent,
     ContactComponent,
-    GalleryComponent,
     LocationComponent,
     IconComponent,
     CardServicesComponent,
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -46,6 +47,8 @@ import { CardServicesComponent } from './components/card-services/card-services.
     MatFormFieldModule,
     FormsModule,
     ReactiveFormsModule,
+    GalleryModule,
+    GalleryComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
