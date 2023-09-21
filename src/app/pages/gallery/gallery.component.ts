7b649f2c912bb89cf875b476836ffcf1a0e9088e
paulolatta <paulogn.lata@gmail.com>
2023-09-21 17:57:37 -0300

    fix issue_866axdtun add simple gallery page

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Gallery, GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
+  standalone: true,
+  imports: [GalleryModule, MatIconModule],
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
+  items = [
+    'caixa.webp',
+    'aterramento.webp',
+    'encanamento.webp',
+    'geral.webp',
+    'imagem-carlos.webp',
+    'painel.webp',
+    'serve-lar.webp',
+    'Joape-6-litros.jpg',
+    'Manutencao-preventiva-eletrica.jpg',
+  ];
 
+  constructor(private gallery: Gallery) {}
+
+  ngOnInit() {
+    const galleryRef = this.gallery.ref('myGallery');
+
+    for (let item of this.items) {
+      galleryRef.addImage({
+        src: `./assets/images/${item}`,
+        thumb: `./assets/images/${item}`,
+      });
+    }
+  }
 }
