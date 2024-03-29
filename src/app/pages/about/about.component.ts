d87df58fd5d937eb705c69b221f8052f65721fc3
paulolatta <paulogn.lata@gmail.com>
2024-03-29 12:58:22 -0300

    add sobre

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
-  selector: 'app-about',
-  standalone: true,
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+    selector: 'app-about',
+    standalone: true,
+    templateUrl: './about.component.html',
+    styleUrl: './about.component.scss',
+    imports: [FooterComponent]
 })
 export class AboutComponent {
 
